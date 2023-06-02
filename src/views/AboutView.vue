b32f3343c06dd7535ed0b81094aafa2524922653
2750056996 <101922124+2750056996@users.noreply.github.com>
2023-06-02 09:44:56 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,34 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>用户看到的界面</h1>
   </div>
 </template>
 
 <script>
 
-export default {
-  name: "AboutView",
-  data() {
-    return {
-      booknum: {
-        pageSize: 50,
-        currentPage: 1,
-      },
-    };
-  },
-  created() {
-    this.projectlist(this.booknum)
-  },
-  methods: {
-    projectlist: function (booknum) {
-      this.axios
-        .get("/api/manage/queryAll", {
-          params: { pageSize: booknum.pageSize, currentPage:booknum.currentPage },
-        })
-        .then((resp) => {
-          console.log(resp.data);
-        });
-    },
-  },
-};
 </script>
